08ecc43aaff3c9a548ab3771be8e187f7209b443
Muhammad Faisal <136161964+Faisal25DEC@users.noreply.github.com>
2023-09-23 01:01:33 +0530

    Time: 1 ms (89.69%) | Memory: 40.2 MB (80.22%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j= 0;
+        for(;i<s.length() && j<t.length();j++){
+            if(s.charAt(i) == t.charAt(j))i++;
+        }
+        return i == s.length();
+    }
+}
