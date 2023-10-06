57b52ad89fc80082ea703e838afb55888bb1ba5f
Muhammad Faisal <136161964+Faisal25DEC@users.noreply.github.com>
2023-10-07 02:18:41 +0530

    Time: 1 ms (61.37%) | Memory: 39.3 MB (39.69%) - LeetSync

---
+++ 343-integer-break/integer-break.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int integerBreak(int n) {
+        int [] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for(int i= 3;i<dp.length;i++){
+            for(int j = 1;j<i;j++){
+                dp[i] = Math.max(dp[i],j*(Math.max(i-j,dp[i-j])));
+            }
+        }
+        return dp[n];
+    }
+}
