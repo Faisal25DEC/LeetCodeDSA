2187f03380f4a31ceda3d28cf8fbbacfca7b0b0a
Muhammad Faisal <136161964+Faisal25DEC@users.noreply.github.com>
2023-10-20 02:09:32 +0530

    Time: 3 ms (14.44%) | Memory: 6.4 MB (6.37%) - LeetSync

---
+++ 1033-broken-calculator/broken-calculator.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        int count = 0;
+        while(startValue<target){
+            if(target%2 == 0)target/=2;
+            else{
+               target++;
+            }
+            count++;
+        }
+        count+=(startValue-target);
+        return count;
+    }
+};
